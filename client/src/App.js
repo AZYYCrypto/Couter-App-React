6a6bf9af999ed69596a2080e59863807daa06c8e
Dimitar <wopz1337@gmail.com>
2022-08-18 13:50:09 +0300

    I created the counter in a state with a useState hook and added 2 buttons to change the state value

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [Counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <button
+        type="click"
+        onClick={() => {
+          setCounter(Counter - 1);
+        }}
+      >
+        -
+      </button>
+      <p>{Counter}</p>
+      <button
+        type="click"
+        onClick={() => {
+          setCounter(Counter + 1);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
 }
 
 export default App;
