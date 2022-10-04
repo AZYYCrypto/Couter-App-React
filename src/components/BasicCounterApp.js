df334e4286f52a258355121dea11d86f65cbd8ec
Dimitar <wopz1337@gmail.com>
2022-10-04 10:29:16 +0300

    Update Counter App - Added initialState & Added Reset Button + Functionality

--- src/components/BasicCounterApp.js
+++ src/components/BasicCounterApp.js
@@ -1,32 +1,43 @@
 import { useState } from "react";
-
+const initialState = 0;
 const BasicCounterApp = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(initialState);
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
         marginTop: "1rem",
         gap: "1rem",
       }}
     >
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <button
+          type="click"
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          -
+        </button>
+        <p>{counter}</p>
+        <button
+          type="click"
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
       <button
         type="click"
         onClick={() => {
-          setCounter(counter - 1);
+          setCounter(initialState);
         }}
       >
-        -
-      </button>
-      <p>{counter}</p>
-      <button
-        type="click"
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        +
+        Reset
       </button>
     </div>
   );
